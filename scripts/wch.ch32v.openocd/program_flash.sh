febe7ca45fc24b55e28e3179513522bdb3a24d56
null <me@github0null.io>
2022-12-11 14:27:40 +0800

    add wch_openocd

---
+++ scripts/wch.ch32v.openocd/program_flash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+firmwareFile=$(echo $1 | sed 's/\\/\//g')
+
+./tools/wch_openocd/bin/openocd.exe -f wch-riscv.cfg \
+    -c init -c halt \
+    -c "flash erase_sector wch_riscv 0 last" \
+    -c "program ${firmwareFile}" \
+    -c "verify_image ${firmwareFile}" \
+    -c wlink_reset_resume -c exit
